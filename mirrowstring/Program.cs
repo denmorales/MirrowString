3f7e7ebb0f300d3a1a817e7d69a0c9c022590c01
Matveev <MatveevDU@mpei.ru>
2021-02-05 18:16:16 +0300

    Add project files.

---
+++ mirrowstring/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace mirrowstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            int n;
+            string stroka1, stroka2;
+            string stroka1out = "";
+            string stroka2out = "";
+            string stroka1Vvod = Console.ReadLine();
+
+             n = stroka1Vvod.Length;
+             for (int i = 0; i < n; i++)
+             {
+                 stroka1 = stroka1Vvod.Substring(n - i - 1, 1);
+                 stroka1out += stroka1;
+             }
+             Console.WriteLine(stroka1out);
+
+
+            //part 2
+
+            string stroka2Vvod = Console.ReadLine();
+            string[] mass = stroka2Vvod.Split('/');
+            int dlinamas = mass.Length;
+            for (int j = 0; j < dlinamas; j++)
+            {
+                n = mass[j].Length;
+
+                for (int i = 0; i < n; i++){ 
+                    stroka2 = mass[j].Substring(n - i - 1, 1); 
+                    stroka2out += stroka2;
+
+                }
+                if (j!=(dlinamas-1))
+                { 
+                    stroka2out += "/";
+                }
+                
+
+
+
+            }
+            Console.WriteLine(stroka2out);
+
+        }
+        
+
+    }
+}
